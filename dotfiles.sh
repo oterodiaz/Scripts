7720b0cd4608814ca03fc36371aaa86646865b10
Otero Díaz <4396445-oterodiaz@users.noreply.gitlab.com>
2022-06-26 15:49:24 +0200

    Avoid opening an empty file when the user cancels

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,5 +3,5 @@
 DOTFILES="$(git --git-dir $HOME/.dotfiles ls-files | sed 's|^|~/|')"
 SCRIPTS="$(git --git-dir $SCRIPTS/.git ls-files | sed "s|^|$SCRIPTS/|" | sed "s|$HOME|~|")"
 
-CHOICE="$(printf '%s\n%s' "$DOTFILES" "$SCRIPTS" | rofi.sh -dmenu -i -p 'Choose a file')"
+CHOICE="$(printf '%s\n%s' "$DOTFILES" "$SCRIPTS" | rofi.sh -dmenu -i -p 'Choose a file')" || exit 1
 alacritty -e sh -c "$EDITOR $CHOICE"
